a27386099cc57e46d8f2cca8b370a21b5d8c0ef2
Shuping <shuping@pdx.edu>
2018-01-20 14:37:25 -0800

    change wrapperregister to register

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ export class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <WrappedRegister/>
+                <Register/>
             </div>
         );
     }
