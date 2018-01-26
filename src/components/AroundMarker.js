f0d66a0477043ec29d71efa5072705e64a2ad92a
Shuping <shuping@pdx.edu>
2018-01-26 11:40:04 -0800

    add mouseOver and mouseOut

--- src/components/AroundMarker.js
+++ src/components/AroundMarker.js
@@ -17,16 +17,13 @@ export class AroundMarker extends React.Component {
     return (
       <Marker
           position={{lat: location.lat, lng: location.lon}}
-          onClick={this.onToggleOpen}
+          onMouseOver={this.onToggleOpen}
+          onMouseOut={this.onToggleOpen}
       >
         {this.state.isOpen ?
             <InfoWindow onCloseClick={this.onToggleOpen}>
               <div>
-                <img
-                    className="around-marker-image"
-                    scr={url}
-                    alt={`${user}: ${message}`}
-                />
+                <img className="around-marker-image" scr={url} alt={`${user}: ${message}`}/>
                 <p>{`${user}: ${message}`}</p>
               </div>
             </InfoWindow> : null}
